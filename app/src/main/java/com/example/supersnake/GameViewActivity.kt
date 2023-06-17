0fe6d0b27dad485bfa280f8deb1fa4b1afc9b0d4
moritzmusel <mormusel@edu.aau.at>
2023-06-17 11:57:33 +0200

    init ui and game activity. ui nav graph. Machts was damit ihr geilen schnitten

---
+++ app/src/main/java/com/example/supersnake/GameViewActivity.kt
@@ -0,0 +1,13 @@
+package com.example.supersnake
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_view)
+
+
+    }
+}
